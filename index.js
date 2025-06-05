826e16cf7a8a516d0a557eaa2e5eb9f176ad07f2
vishal yadav <115427570+iam-vishal-ydv@users.noreply.github.com>
2025-06-05 15:28:14 +0530

    fix url issue

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", userRoutes);
 app.use(
   cors({
-    origin: "https://ecover-se.vercel.app/",
+    origin: ["http://localhost:5173", "https://ecover-se.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
