74e9abd8ac08e8495e7a95bdfccfe4e679a03301
vishal yadav <115427570+iam-vishal-ydv@users.noreply.github.com>
2025-06-05 15:33:24 +0530

    add blacklisting

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", userRoutes);
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ecover-se.vercel.app"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
   })
 );
 
