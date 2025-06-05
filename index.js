dd0e241e0d03c6c699428ae03e4b07d7b13f8141
vishal yadav <115427570+iam-vishal-ydv@users.noreply.github.com>
2025-06-05 13:33:53 +0530

    chnage port

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const port = process.env.PORT_NO || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
   console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
